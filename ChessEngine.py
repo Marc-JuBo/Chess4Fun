7d5c231c7f8d9431749b85ceeda0e70a43dc3bcf
Marc-JuBo <114589109+Marc-JuBo@users.noreply.github.com>
2022-09-28 22:29:37 +0200

    New update. Pawn possible moves
    Today i have given the pawn his options to move: 1 straight, 2 straight, and the option to kill if there's a piece diagonally at 1 step.
    Tomorrow probably will make him actually move :)

--- ChessEngine.py
+++ ChessEngine.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 """
 - té tota la informació de l'estat de joc
 - responsable de determinar els moviments vàlids
@@ -42,3 +43,106 @@ class Move():
 
 def tryPossible(gs):
     pass
+=======
+"""
+- té tota la informació de l'estat de joc
+- responsable de determinar els moviments vàlids
+- tindrà un log dels moviments
+"""
+
+from Parameters import *
+
+from http.client import SWITCHING_PROTOCOLS
+
+
+class GameState():
+    def __init__(self):
+        # Tauler actual
+        self.whiteDown = True
+        if(self.whiteDown):
+            self.board = [
+                ["bR", "bp", "--", "--", "--", "--", "wp", "wR"],
+                ["bN", "bp", "--", "--", "--", "--", "wp", "wN"],
+                ["bB", "bp", "--", "--", "--", "--", "wp", "wB"],
+                ["bQ", "bp", "--", "--", "--", "--", "wp", "wQ"],
+                ["bK", "bp", "--", "--", "--", "--", "wp", "wK"],
+                ["bB", "bp", "--", "--", "--", "--", "wp", "wB"],
+                ["bN", "bp", "--", "--", "--", "--", "wp", "wN"],
+                ["bR", "bp", "--", "--", "--", "--", "wp", "wR"],
+            ]
+
+        else:
+            self.board = [
+                ["wR", "wp", "--", "--", "--", "--", "bp", "bR"],
+                ["wN", "wp", "--", "--", "--", "--", "bp", "bN"],
+                ["wB", "wp", "--", "--", "--", "--", "bp", "bB"],
+                ["wQ", "wp", "--", "--", "--", "--", "bp", "bQ"],
+                ["wK", "wp", "--", "--", "--", "--", "bp", "bK"],
+                ["wB", "wp", "--", "--", "--", "--", "bp", "bB"],
+                ["wN", "wp", "--", "--", "--", "--", "bp", "bN"],
+                ["wR", "wp", "--", "--", "--", "--", "bp", "bR"],
+            ]
+
+        # Peça seleccionada, i tauler (binari) de possibles moviments d'aquesta peça
+        self.kingHasMoved = False
+        self.pieceSelected = False 
+        self.sqSelected = () #és un log de la última (row,col) que ha premut el jugador
+        self.posible = [[0 for col in range(8)] for row in range(8)]
+         # Mostra les possibles opcions de moviment:
+            # 0 no té opció d'arribar-hi
+            # 1 té opció d'arribar-hi
+            # 2 té opció de capturar peça
+
+        self.whiteToMove = True
+        self.moveLog = []
+
+        
+def tryPosible(gs):
+    gs.posible = [[0 for col in range(8)] for row in range(8)]
+    sqc = gs.sqSelected[0]
+    sqr = gs.sqSelected[1]
+    piece =  gs.board[gs.sqSelected[0]][gs.sqSelected[1]]
+    # Ara mirarem peça per peça, tots els casos. 
+    pieceColor = piece[0]
+    if pieceColor == 'w': 
+        enemyColor = 'b' 
+    else: 
+        enemyColor = 'w'          
+    if piece[1] == 'p': # Comencem mirant els peons.
+        if (gs.whiteDown and pieceColor == 'w') or (not gs.whiteDown and pieceColor == 'b'): 
+            # Mirem primer els peons de sota.
+            if(gs.board[sqc][sqr-1] == "--"):
+                gs.posible[sqc][sqr-1] = 1
+                if(sqr == 6 and gs.board[sqc][sqr-2] == "--"): 
+                    gs.posible[sqc][sqr-2] = 1
+            if(gs.board[sqc+1][sqr-1][0] == enemyColor and sqr != 7):
+                gs.posible[sqc+1][sqr-1] = 2
+            if(gs.board[sqc-1][sqr-1][0] == enemyColor and sqr != 0):
+                gs.posible[sqc-1][sqr-1] = 2  
+        else:
+        # Mirem ara els peons de sobre.
+            if(gs.board[sqc][sqr+1] == "--"):
+                gs.posible[sqc][sqr+1] = 1
+                if(sqr == 1 and gs.board[sqc][sqr+2] == "--"): 
+                    gs.posible[sqc][sqr+2] = 1
+            if(gs.board[sqc+1][sqr+1][0] == enemyColor and sqr != 7):
+                gs.posible[sqc+1][sqr+1] = 2
+            if(gs.board[sqc-1][sqr+1][0] == enemyColor and sqr != 0):
+                gs.posible[sqc-1][sqr+1] = 2  
+            
+
+        
+
+    
+
+class Move():
+    def __init__(self,startSq,endSq,board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow,self.startRow]
+        self.pieceCaptured = board[self.endRow,self.endCol]
+
+
+>>>>>>> Stashed changes
--- ChessMain.py
+++ ChessMain.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 """
 Usuari fa les decisions
 """
@@ -88,4 +89,105 @@ def drawPieces(screen,gs):
 
 
 if __name__ == "__main__":
+=======
+"""
+Usuari fa les decisions
+"""
+
+import pygame as p
+from Parameters import *
+import ChessEngine
+from ChessEngine import *
+
+'''
+inicialització del diccionari d'imatges
+'''
+def loadImages():
+    pieces = ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR","wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR","bp","wp"]
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load(".\\Chess4Fun\\images\\"+piece+".png"),(SQ_SIZE,SQ_SIZE))
+
+'''
+usuari inputa els gràfics
+'''
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH,HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("White"))
+    gs = ChessEngine.GameState()
+    loadImages()
+    running = True
+    playerClicks = [] #log dels clics que ha fet el jugador en tuples ex: ([(6,4),(5,4)])
+
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos() # ens dona un vector de coords (x,y)
+                row = location[0]// SQ_SIZE
+                col = location[1]// SQ_SIZE
+                if gs.sqSelected == (row,col): #l'usuari clica el mateix quadre (esborrar selecció)
+                    gs.sqSelected = ()
+                    gs.pieceSelected = False
+                    gs.posible = [[0 for col in range(8)] for row in range(8)] 
+                    playerClicks = []
+                else:
+                    if( (gs.board[row][col][0] == 'w' and gs.whiteToMove) or (gs.board[row][col][0] == 'b' and not gs.whiteToMove)): 
+                        gs.sqSelected = (row,col)
+                        gs.pieceSelected = True
+                        tryPosible(gs)
+                        playerClicks.append(gs.sqSelected) #append per clic dret i esquerre
+                if len(playerClicks) == 2: # quan es clica la casella on es vol portar la peça
+                    
+                    pass
+
+
+
+        drawGameState(screen,gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+
+'''
+responsable dels gràfics d'un gamestate
+'''
+def drawGameState(screen,gs):
+    drawBoard(screen, gs)
+    drawOptions(screen, gs)
+    drawPieces(screen, gs)
+
+def drawBoard(screen, gs):
+    colors=[p.Color("white"),p.Color("gray")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[(r+c)%2]
+            p.draw.rect(screen,color,p.Rect(r*SQ_SIZE,c*SQ_SIZE,SQ_SIZE,SQ_SIZE))
+    if gs.pieceSelected: 
+        p.draw.rect(screen,"yellow",p.Rect(gs.sqSelected[0]*SQ_SIZE,gs.sqSelected[1]*SQ_SIZE,SQ_SIZE,SQ_SIZE))
+                
+
+def drawOptions(screen, gs):
+    colors=[p.Color("white"),p.Color("gray")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            if(gs.posible[r][c] == 1):
+                p.draw.circle(screen, "lightgray",((r+0.5)*SQ_SIZE, (c+0.5)*SQ_SIZE), SQ_SIZE*0.3)
+            if(gs.posible[r][c] == 2): 
+                p.draw.circle(screen, "lightgray",((r+0.5)*SQ_SIZE, (c+0.5)*SQ_SIZE), SQ_SIZE*0.5)
+                p.draw.circle(screen, colors[(r+c)%2],((r+0.5)*SQ_SIZE, (c+0.5)*SQ_SIZE), SQ_SIZE*0.4)
+
+
+def drawPieces(screen,gs):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = gs.board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece],p.Rect(r*SQ_SIZE,c*SQ_SIZE,SQ_SIZE,SQ_SIZE))
+
+
+if __name__ == "__main__":
+>>>>>>> Stashed changes
     main()
